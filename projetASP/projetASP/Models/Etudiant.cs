5b1acc003624c6301133f7c681576483f00360e2
DefaultLol <47484719+DefaultLol@users.noreply.github.com>
2020-02-23 00:07:22 +0100

    Adding Etudiant model

---
+++ projetASP/projetASP/Models/Etudiant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace projetASP.Models
+{
+    public class Etudiant
+    {
+        public int id { get; set; }
+        [Required]
+        public string cin { get; set; }
+        [Required]
+        public string cne { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string password { get; set; }
+        public DateTime date_naissance { get; set; }
+        public string filiere { get; set; }
+        public int tele { get; set; }
+        public string address { get; set; }
+        public string ville { get; set; }
+        public string prev_etablissement { get; set; }
+        public string type_diplome { get; set; }
+        public int note { get; set; }
+
+    }
+}
